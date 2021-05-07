a271ab9712f9d0e84ebffedb18e6152d7316e760
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-05-07 16:12:58 -0400

    this is not working completly did not spend enough time on it, shouldve been easy, I need to review some material

--- spaceX.js
+++ spaceX.js
@@ -2,8 +2,59 @@ const engineers = [{
     title: 'CEO/CTO',
     name: 'Elon Musk',
     age: 49,
-    yearsWithsx: 19
-    
+    yearsWithsx: 19,
+    about: 'Elon Reeve Musk FRS is a business magnate, industrial designer, and engineer. He is the founder, CEO, CTO, and chief designer of SpaceX; early stage investor, CEO, and product architect of Tesla, Inc.; founder of The Boring Company; co-founder of Neuralink; and co-founder and initial co-chairman of OpenAI',
+},{
+    title:'Rocket engine engineer',
+    name:'Tom Mueller',
+    age: 57,
+    yearsWithsx: 19,
+    about:'Thomas Mueller is an American rocket engineer and rocket engine designer. He was a founding employee of SpaceX, an American aerospace manufacturer and space transportation services company headquartered in Hawthorne, California. He is best known for his engineering work on the TR-106 and SpaceX rocket engines'
+},{
+    title:'COO',
+    name:'Gwynne Shotwell',
+    age: 57,
+    yearsWithsx: 18,
+    about:'Gwynne Shotwell is an American businesswoman and engineer. She is the president and chief operating officer of SpaceX, an American space transportation company, where she\'s responsible for day-to-day operations and company growth. As of 2020, she is listed as the 49th most powerful woman in the world by Forbes.'
+},{
+    title:'Director of Space Operations',
+    name:'Garret Reisman',
+    age: 53,
+    yearsWithsx: 10, 
+    about:'Garrett Erin Reisman is an American engineer and former NASA astronaut. He was a backup crew member for Expedition 15 and joined Expedition 16 aboard the International Space Station for a short time before becoming a member of Expedition 17.'
+},{
+    title:'Aerospace Engineer/Astronout',
+    name:'Thomas Pesquet',
+    age: 43,
+    yearsWithsx: 10,
+    about:'Thomas Gautier Pesquet is a French aerospace engineer, pilot, and European Space"Age"ncy astronaut. Pesquet was selected by ESA as a candidate in May 2009, and he successfully completed his basic training in November 2010'
+},{
+    title:'Astronaut',
+    name:'Bob Behnken',
+    age:50,
+    yearsWithsx:5,
+    about:'Robert Louis Behnken is a NASA astronaut, engineer, and former Chief of the Astronaut Office. Behnken holds a Ph.D in mechanical engineering and the rank of colonel in the U.S. Air Force, where he served before joining NASA in 2000.'
+},{
+    title:'Astronaut',
+    name:'Doug Hurley',
+    age: 54,
+    yearsWithsx:5,
+    about:'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and current NASA astronaut. He piloted space shuttle missions STS-127 and STS-135, the final flight of the space shuttle program.'
+},{
+    title:'Astronaut/Aeronautical Engineer',
+    name:'Soichi Noguchi',
+    age: 56,
+    yearsWithsx: 5,
+    about:'Soichi Noguchi is a Japanese aeronautical engineer and JAXA astronaut. His first spaceflight was as a Mission Specialist aboard STS-114 on 26 July 2005 for NASA\'s first "\return to flight\" Space Shuttle mission after the Columbia disaster'
+},{
+    title:'Flight Engineer',
+    name:'Sergey KudSverchkov',
+    age: 37,
+    yearsWithsx:5,
+    about:'Sergey Vladimirovich Kud-Sverchkov is a Russian cosmonaut, selected in 2010 by Roscosmos. He made his first spaceflight in 2020 aboard the International Space Station as a flight engineer for ISS Expedition 63/64'
 
 }]
+
+module.exports = engineers
+
 
